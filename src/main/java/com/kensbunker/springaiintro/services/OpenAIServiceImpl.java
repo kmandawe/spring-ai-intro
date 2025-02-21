4e08217cd5dbf1d25628778f9f72c63100dd2770
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-02-21 19:18:00 +0800

    Directing OpenAI to respond with JSON

--- src/main/java/com/kensbunker/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/com/kensbunker/springaiintro/services/OpenAIServiceImpl.java
@@ -1,5 +1,9 @@
 package com.kensbunker.springaiintro.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kensbunker.springaiintro.model.Answer;
 import com.kensbunker.springaiintro.model.GetCapitalRequest;
 import com.kensbunker.springaiintro.model.Question;
@@ -8,6 +12,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -23,8 +28,11 @@ public class OpenAIServiceImpl implements OpenAIService {
   @Value("classpath:templates/get-capital-with-info.st")
   private Resource getCapitalPromptWithInfo;
 
-  public OpenAIServiceImpl(ChatModel chatModel) {
+  private final ObjectMapper objectMapper;
+
+  public OpenAIServiceImpl(ChatModel chatModel, ObjectMapper objectMapper) {
     this.chatModel = chatModel;
+    this.objectMapper = objectMapper;
   }
 
   @Override
@@ -48,18 +56,28 @@ public class OpenAIServiceImpl implements OpenAIService {
 
   @Override
   public Answer getCapital(GetCapitalRequest getCapitalRequest) {
-    PromptTemplate promptTemplate =
-        new PromptTemplate(getCapitalPrompt);
-    Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
+    PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
+    Prompt prompt =
+        promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
     ChatResponse response = chatModel.call(prompt);
-    return new Answer(response.getResult().getOutput().getText());
+    String textResponse = response.getResult().getOutput().getText();
+    System.out.println(textResponse);
+    String responseString;
+    try {
+      JsonNode jsonNode =
+          objectMapper.readTree(textResponse.replaceAll("```json", "").replaceAll("```", ""));
+      responseString = jsonNode.get("answer").asText();
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+    return new Answer(responseString);
   }
 
   @Override
   public Answer getCapitalWithInfo(GetCapitalRequest getCapitalRequest) {
-    PromptTemplate promptTemplate =
-        new PromptTemplate(getCapitalPromptWithInfo);
-    Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
+    PromptTemplate promptTemplate = new PromptTemplate(getCapitalPromptWithInfo);
+    Prompt prompt =
+        promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
     ChatResponse response = chatModel.call(prompt);
     return new Answer(response.getResult().getOutput().getText());
   }
